5592a2d1ac14e803dd7f167e1b9c07221a954bac
Alexander <sanowese@gmail.com>
2024-06-16 23:32:57 +0200

    Sound update

--- modules/auth.py
+++ modules/auth.py
@@ -1,13 +1,20 @@
 from utils.page import BasePage
 from pygame.event import Event
+from pygame.mixer import Sound
 
 from utils.start_app import start_app
+from time import sleep
 
 
 class AuthPage(BasePage):
 
     def __init__(self):
         super().__init__()
+
+        sound = Sound('assets/sounds/auth/loaded.mp3')
+        sound.play()
+        sleep(4)
+        sound.stop()
 
     def draw(self):
         text = self.DEFAULT_FONT.render('Auth page', True, self.thema.text)
@@ -18,7 +25,9 @@ class AuthPage(BasePage):
         pass
 
     def exit_event(self):
-        pass
+        sound = Sound('assets/sounds/auth/app_shutdown.mp3')
+        sound.play()
+        sleep(1)
 
 
 if __name__ == "__main__":
