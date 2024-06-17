8adb740cd44aa3deab14a92c632f4f7f1d04a8f6
Alexander <sanowese@gmail.com>
2024-06-17 11:36:51 +0200

    Config update

--- utils/config.py
+++ utils/config.py
@@ -6,6 +6,17 @@ class Config:
 
     WHITE = (255, 255, 255)
     BLACK = (0, 0, 0)
+    RED = (255, 0, 0)
+    GREEN = (0, 255, 0)
+    BLUE = (0, 0, 255)
+    YELLOW = (255, 255, 0)
+    CYAN = (0, 255, 255)
+    MAGENTA = (255, 0, 255)
+    GRAY = (128, 128, 128)
+    ORANGE = (255, 165, 0)
+    PURPLE = (128, 0, 128)
+    PINK = (255, 192, 203)
+    BROWN = (165, 42, 42)
 
     def __init__(self, config_file='config.json'):
         self.config_file = config_file
