4139074822b3e616c4f09cd14eb96c9a88b561fa
Enrique <enriquebaltes@outlook.com>
2024-06-18 14:24:18 +0200

    Menu button configuration

--- pages/menu.py
+++ pages/menu.py
@@ -1,24 +1,34 @@
+import typing
+import pygame
 from utils.page import BasePage
 from pygame.event import Event
+from utils.button import Button
 import pygame_menu
 from utils.start_app import start_app
+from pages.auth import AuthPage
 
 
 class MenuPage(BasePage):
 
+
     def __init__(self):
         super().__init__()
-
+        self.buttons: typing.List[Button] = [
+            Button(100, 100, 100, 20, "Welcome!", self.thema.text, background=self.thema.background)
+        ]
     def draw(self):
-        text = self.DEFAULT_FONT.render('Menu page', True, self.thema.button)
-        text_rect = text.get_rect(center=(self.SCREEN.get_width() // 2, self.SCREEN.get_height() // 2))
-        self.SCREEN.blit(text, text_rect)
+        for button in self.buttons:
+            button.draw(self.SCREEN)
     def handle_event(self, event: Event):
-        pass
-
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            print("Button was clicked")
+            self.log_out_button_handler()
     def exit_event(self):
-        pass
+        print("Hello, Alex!")
 
+    def log_out_button_handler(self):
+        authPage = AuthPage()
+        authPage.set_as_current_page()
 
 if __name__ == "__main__":
     start_app(MenuPage())
