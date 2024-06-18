1669e3e22620dff2708840bf46b4fea76e57c062
Alexander <sanowese@gmail.com>
2024-06-18 11:13:43 +0200

    Test

--- models/user.py
+++ models/user.py
@@ -4,7 +4,7 @@ from utils.model import BaseModel
 
 class UserModel(BaseModel):
     def __init__(self):
-        super().__init__() # BLABLABLA
+        super().__init__() # ABC ABC ABC
         self._username = None
 
     def initialize_table(self):
