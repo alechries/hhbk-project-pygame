66b9b7bab23612615dcc0a0bf26f32ebc9a67854
Alexander <sanowese@gmail.com>
2024-06-19 11:42:14 +0200

    Update

---
+++ utils/types.py
@@ -0,0 +1,15 @@
+class Team:
+
+    WHITE_TEAM = 'white_team'
+    BLACK_TEAM = 'black_team'
+class Game:
+    CHESS_GAME = 'chess_game'
+    CHECKERS_GAME = 'checkers_game'
+    TICTACTOE_GAME = 'tictactoe_game'
+class Level:
+    a = 'a'
+    b = 'b'
+    c = 'c'
+class Spawn:
+    TOP_SPAWN = 'top_spawn'
+    BOTTOM_SPAWN = 'bottom_spawn'
