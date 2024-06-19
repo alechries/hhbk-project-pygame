89f882e6fdf5a9217610e46883b28240df7407a1
Alexander <sanowese@gmail.com>
2024-06-19 11:48:33 +0200

    EnumsÍ

--- utils/types.py
+++ utils/types.py
@@ -1,27 +1,27 @@
 from enum import IntEnum
 
 
-class Team(IntEnum):
+class TeamType(IntEnum):
 
     WHITE_TEAM = 1
     BLACK_TEAM = 2
 
 
-class Game(IntEnum):
+class GameType(IntEnum):
 
     CHESS_GAME = 1
     CHECKERS_GAME = 2
     TICTACTOE_GAME = 3
 
 
-class Level(IntEnum):
+class LevelType(IntEnum):
 
     EASY = 1
     MEDIUM = 2
     HARD = 3
 
 
-class Spawn(IntEnum):
+class SpawnType(IntEnum):
 
     TOP_SPAWN = 1
     BOTTOM_SPAWN = 2
