a0c707cef412d9e561ed396623fdaf2d1abad656
Necoris <45900537+Necoris@users.noreply.github.com>
2024-06-21 11:50:23 +0200

    Add files via upload

--- utils/algorithm.py
+++ utils/algorithm.py
@@ -1,21 +1,24 @@
-#initialize_board -> Das Board wird geladen
-#get_valid_moves -> Regelset für die Figuren
+#initialize_board -> Das Board wird geladen -> get_current_map_with_pieces
+#get_valid_moves -> Regelset für die Figuren -> get_valid_moves_inside
 #make_move -> Die Bewegung wird ausgeführt
 #evaluate_board -> Das neue Board wird übertragen
-#is_terminal_state -> Ist das Spiel schon gewonnen?
+#is_terminal_state -> Ist das Spiel schon gewonnen? -> check_winner
+import pygame
+from copy import deepcopy
+
+from utils import board
 
 
 class Algorithm:
     def __init__(self):
-        self.board = self.initialize_board()
+        self.board = board.get_current_map_with_pieces()
         self.current_turn = 1  # 1 for player 1, -1 for player 2
 
-
     def minimax(self, board, depth, maximizing_player):
-        if depth == 0 or self.is_terminal_state(board):
+        if depth == 0 or board.check_winner(board):
             return self.evaluate_board(board), None
 
-        valid_moves = self.get_valid_moves(board, 1 if maximizing_player else -1)
+        valid_moves = board.get_valid_moves_inside(board, 1 if maximizing_player else -1)
 
         if maximizing_player:
             max_eval = float('-inf')
@@ -37,7 +40,6 @@ class Algorithm:
                     min_eval = eval
                     best_move = move
             return min_eval, best_move
-
 
     def find_best_move(self, depth):
         _, best_move = self.minimax(self.board, depth, True if self.current_turn == 1 else False)
